89550d504c4a16d52bd35725a62c0e5c108fab5b
Ping Guan <ping.guan@outlook.com>
2022-02-13 14:16:13 -0700

    update

--- src/js/delete.js
+++ src/js/delete.js
@@ -1,9 +1,21 @@
-function pageInit(){
-    const key = sessionStorage.getItem('key')
-    console.log("delete page")
-    console.log(key)
-    // ref with the key
-    // remove(ref)
-  }
-  
-  pageInit()
+import {ref as databaseRef, push, set, get} from 'firebase/database';
+import {db, storage} from "./libs/firebase/firebaseConfig";
+import {doc, deleteDoc } from "firebase/firestore";
+
+
+async function pageInit() {
+    const key = sessionStorage.getItem('key');
+    const itemRef =  databaseRef( db, `products/${key}`);
+    const productRef = databaseRef(db,`products/${key}`);
+    const productSnapShot = await get(productRef);
+
+    set(itemRef,{
+        
+     });
+    
+
+    window.location.href = "index.html";
+
+}
+
+pageInit();
